50796143988575ded435bcf25ce3699f9b909007
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-02-04 19:14:35 +0530

    Ui Changes

--- Js/new.js
+++ Js/new.js
@@ -1,14 +1,19 @@
 let ansCount = document.getElementById("count")
 let numCount = 0
 let saveEl = document.getElementById("save-el")
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes();
+
 function increment(){
     numCount += 1
     ansCount.innerHTML = numCount
 }
 
 function save(){
-    let newans = numCount + " - "
-    saveEl.innerHTML += newans
+    let newans = time + " :- " + numCount + " || "
+    saveEl.innerText += newans
+    ansCount.innerText = 0
+    numCount = 0
 }
 
 
